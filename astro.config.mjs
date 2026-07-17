0613e2e6714d4042a69a0da8bb529ed665b63d58
Paya Maroufi <h2o.Drop2010@gmail.com>
2026-07-16 18:20:26 -0700

    feat: Initialize astro project

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+// @ts-check
+import { defineConfig, passthroughImageService } from 'astro/config';
+
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://DCBMUN.github.io',
+	base: '/',
+	integrations: [react(), icon()],
+
+	vite: {
+		plugins: [tailwindcss()]
+	},
+
+	image: {
+		service: passthroughImageService(),
+	},
+});
